6642c56a83c56ed3ca0c0fe58d0f86f492da4496
rhill34 <rhill34@mail.greenriver.edu>
2020-06-19 09:47:39 -0700

    fix : removed unnecessary code

--- TrackerLibrary/MatchupEntryModel.cs
+++ TrackerLibrary/MatchupEntryModel.cs
@@ -5,8 +5,20 @@
     /// </summary>
     public class MatchupEntryModel
     {
+        /// <summary>
+        /// Represents the one team in the this matchup
+        /// </summary>
         public TeamModel TeamCompeting { get; set; }
+
+        /// <summary>
+        /// Represents the score of this team in the mathcup
+        /// </summary>
         public double Score { get; set; }
-        public object MyProperty { get; set; }
+
+        /// <summary>
+        /// Represents the matchup that this
+        /// team came from as the winner
+        /// </summary>
+        public MatchupModel ParentMatchup { get; set; }
     }
 }
--- TrackerLibrary/PrizeModel.cs
+++ TrackerLibrary/PrizeModel.cs
@@ -1,5 +1,4 @@
-﻿using System;
-namespace TrackerLibrary
+﻿namespace TrackerLibrary
 {
     /// <summary>
     /// Class that instantiates a Prize 
